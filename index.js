04c15140cf2190c306d908dd4bc1e0835e1c4008
victoryalhj <victoryalhj@gmail.com>
2024-06-26 20:53:38 +0100

    20240626_Javascript study

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ function MakeUpperCase(str) {
 }
 
 MakeUpperCase('Hello')
+
+//For loop (Add all numbers 1~100)
+let sum = 0
+
+for (let i=1; i<=100; i++) {
+  sum = sum+i
+ }
+console.log(sum)
+
+
+//Odd number 1~100
+for(let i=1; i<=100; i+=2) {
+  console.log(i)
+ }
