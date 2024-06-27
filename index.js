7bb07e56f63b94ebea168555a8cbc41e27015316
victoryalhj <victoryalhj@gmail.com>
2024-06-27 23:13:23 +0100

    20240627

--- index.js
+++ index.js
@@ -18,3 +18,16 @@ console.log(sum)
 for(let i=1; i<=100; i+=2) {
   console.log(i)
  }
+
+
+ //3.1부터 50까지 369결과를 프린트하자 ★
+for(let i=1; i<=50; i++) {
+  let stringValue = i.toString()
+  let result = ""
+  for (let j=0; j<stringValue.length; j++){
+    if (stringValue[j] == "3" || stringValue[j] == "6" || stringValue[j] == "9") {
+      result = result + "짝"
+    }
+  }
+  console.log(result.length>0? result:i)
+}
