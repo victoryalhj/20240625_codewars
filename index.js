cb0e3307da8336aea470a09db63984ae49eb65a1
victoryalhj <victoryalhj@gmail.com>
2024-06-28 19:48:18 +0100

    20240628

--- index.js
+++ index.js
@@ -31,3 +31,23 @@ for(let i=1; i<=50; i++) {
   }
   console.log(result.length>0? result:i)
 }
+
+//4.주어진 숫자가 소수이면 true 아니면 false를 출력하는 프로그램을 짜시오.
+//소수 2,3,5,7....(1은 소수가 아님)
+//변수 n을 11로 초기화,이 변수는 소수인지 판단하기위한 숫자
+
+let n = 11
+let isPrime = true
+
+if(n === 1) {
+    isPrime = false
+} else {
+    for(let i=2; i<n; i++) {
+        if(n%i == 0){
+            isPrime = false
+        }
+    }
+    
+}
+
+console.log(isPrime)
