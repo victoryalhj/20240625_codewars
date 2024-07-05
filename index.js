62e127c343516451820380352ae29d5e500a2aec
victoryalhj <victoryalhj@gmail.com>
2024-07-05 21:34:48 +0100

    20240705

--- index.js
+++ index.js
@@ -51,3 +51,25 @@ if(n === 1) {
 }
 
 console.log(isPrime)
+
+
+//1."안녕 내 이름은 제시카야" 라는 문장을 프린트하는 함수 'greet'를 만드시오
+function great () {
+  console.log("안녕 내 이름은 제시카야")
+}
+
+ great()
+
+//2.이름을 매개변수로 전달받아 다양한 이름을 프린트 할수 있게 함수를 수정하시오 예) "안녕 내 이름은 에밀리야", 
+function greet (name) {
+  console.log("안녕 내 이름은 "+name+"야")
+}
+
+//3.매개변수로 전달받은 이름을 반환하게 함수를 수정하시오 ★
+function greet (name) {
+  console.log("안녕 내 이름은 "+name+"야")
+    return name
+}
+
+const name = greet("제니")
+console.log("안녕 내 이름은 "+name+"야")
